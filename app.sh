b99909438df2a7608f7002f229b8fb982e20bddc
Max Renner <rennerom@gmail.com>
2021-07-29 14:59:15 -0500

    added app.sh file to open app from terminal

---
+++ app.sh
@@ -0,0 +1,5 @@
+if which R >/dev/null; then
+	R -e 'source("init.R"); source("data_import.R"); runApp(launch.browser = TRUE)'
+else
+	echo "Cannot find R. Please install/configure R and try again."
+fi
